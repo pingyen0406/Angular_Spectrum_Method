2368162d97e5094c022f8c229dbb526fc6bf28b7
pingyen0406 <s6622156@gmail.com>
2020-07-22 18:49:16 +0800

    Find the maximum value and slice

--- ASM.m
+++ ASM.m
@@ -1,28 +1,21 @@
-%%
-%parameters
+% parameters
 clear all; clc;
-
-c = 299792458;
 lambda0 = 1.55e-6;
-f = c/lambda0;
-omega = 2*pi*f;
 k0 = 2*pi/lambda0;
-
 n = 1 ; %free space
 lambda = lambda0/n;
 k = k0*n;
-%%
-%Define input source field
+f = 50e-6;
 
-%load file
+% Define input source field
+
+% load input E-field file
 S=load('f_50.mat');
 Ey=S.Ey;
 E_i = Ey(:,:,1,2);
-
-%Define 
 E_i = E_i(2:end,:)';
 source_size = size(E_i);
-%% Input Field window
+%%%%%%%%%%%%%%%%%%%%%%%%%%% Input Field window %%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % nx and ny have to be odd, so that no artifacts are introduced when calculating the discrete Fourier transform
 % If they are even, the propagated beam will be shifted from its center, for example
@@ -35,24 +28,40 @@ yA = S.y'; % Array of window points in the y direction
 [XA,YA]=meshgrid(xA,yA); % Aperture grid
 
 
-%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%% Simulation window %%%%%%%%%%%%%%%%%%%%%%%%%%%%
+dx=0.02e-6; % Spacing outside the source field in the x direction
+dy=0.02e-6; % Spacing outside the source field in the y direction
+dx_mid = zeros(1,length(xA));% spacing of the source field in x direction
+dy_mid = zeros(1,length(yA));% spacing of the source field in y direction
+for i=1:length(xA)-1
+    dx_mid(i) = xA(i+1)-xA(i);
+end
+for i=1:length(yA)-1
+    dy_mid(i) = yA(i+1)-yA(i);
+end
 
-%Simulation window
-dx=0.02e-6; % Spacing between sucessive points in the x direction
-dy=0.02e-6; % Spacing between sucessive points in the y direction
+x_width_s = 5*x_width;
+y_width_s = 5*y_width;
+xA_s = cat(2,[xA(1)-2*x_width:dx:xA(1)],xA(2:end-1),[xA(end):dx:xA(end)+2*x_width]);
+yA_s = cat(2,[yA(1)-2*y_width:dy:yA(1)],yA(2:end-1),[yA(end):dy:yA(end)+2*y_width]);
+obj_size=size(meshgrid(xA_s,yA_s)); 
 
-x_width_s = 4*x_width;
-y_width_s = 4*y_width;
-xA_s = cat(2,[xA(1)-1.5*x_width:dx:xA(1)],xA(2:end-1),[xA(end):dx:xA(end)+1.5*x_width]);
-yA_s = cat(2,[yA(1)-1.5*y_width:dy:yA(1)],yA(2:end-1),[yA(end):dy:yA(end)+1.5*y_width]);
-[XA_s,YA_s] = meshgrid(xA_s,yA_s);
+% Combine source field into Computational window
+E0 = zeros(obj_size(1),obj_size(2));
+nx_start = ceil(obj_size(2)/2)-floor(nx/2) ;
+nx_end = ceil(obj_size(2)/2)+floor(nx/2) ;
+ny_start = ceil(obj_size(1)/2)-floor(ny/2) ;
+ny_end = ceil(obj_size(1)/2)+floor(ny/2);
+E0(ny_start:ny_end,nx_start:nx_end)=E_i;
+% plot input source field
+figure(1);
+imagesc(xA_s,yA_s,abs(E0));
+title('Source field');
 
-obj_size=size(XA_s); 
-
-%% Wave-vector domain parameters
+%%%%%%%%%%%%%%%%%%%%% Wave-vector domain parameters %%%%%%%%%%%%%%%%%%%%%%%
 
 Fs_x = obj_size(2)/x_width_s; % Fx axis range
-Fs_y = obj_size(1)/y_width_s; % Fy axis range
+Fs_y = obj_size(1)/y_width_s; % Fy axis range 
 dFx = Fs_x/(obj_size(2)-1); % Sampling spacing in the Fx axis
 dFy = Fs_y/(obj_size(1)-1); % Sampling spacing in the Fy axis
 Fx=-Fs_x/2:dFx:Fs_x/2; % Points in Fx direction
@@ -63,25 +72,8 @@ alpha = lambda.*FX;
 beta2 = lambda.*FY; 
 gamma_cust = sqrt(1 - alpha.^2 - beta2.^2);
 
-%%
-%Combine source field into Computational window
 
-E0 = zeros(obj_size(1),obj_size(2));
-
-nx_start = ceil(obj_size(2)/2)-floor(nx/2) ;
-nx_end = ceil(obj_size(2)/2)+floor(nx/2) ;
-ny_start = ceil(obj_size(1)/2)-floor(ny/2) ;
-ny_end = ceil(obj_size(1)/2)+floor(ny/2);
-
-E0(ny_start:ny_end,nx_start:nx_end)=E_i;
-
-
-figure(1)
-imagesc(abs(E0))
-title('Source field')
-
-%%
-%Agular spectrum method
+%% Agular spectrum method
 tic;
 
 A0 = fftshift(fft2(E0));
@@ -89,14 +81,17 @@ prop_L = 100e-6;
 nz = 101;
 z_list = linspace(0,prop_L,nz);
 E = zeros(obj_size(1),obj_size(2));
+% slice through the focal spot(x,y v.s. propgation distance)
 E_row = zeros(obj_size(2),nz);
 E_col = zeros(obj_size(1),nz);
+plot_gif = false;
+gif_filename = "TEST.gif";
 
 for i=1:nz
-    %transfer function for free space
+    %transfer function in free space
     T = exp(1i*k.*gamma_cust.*(z_list(1,i)));
     
-    %band limiting
+    %Create band-limiting matrix
     ulx = 1/(((2*dFx*z_list(1,i))^2+1)^0.5*lambda);
     uly = 1/(((2*dFy*z_list(1,i))^2+1)^0.5*lambda);
     [~, nx_l] = min(abs(abs(Fx)-ulx));
@@ -107,44 +102,58 @@ for i=1:nz
     if -uly > Fx(1,ny_l)
         ny_l = ny_l+1;
     end
-    
     Band_Limit_Matrix = zeros(obj_size(1),obj_size(2));
     Band_Limit_Matrix(nx_l:obj_size(2)-nx_l+1,ny_l:obj_size(1)-ny_l+1)=1;
-    
     T = T.*Band_Limit_Matrix;
-    
     %Calculation
     E = ifft2(ifftshift(A0.*T));
-    %figure(i+1)
-    %imagesc(abs(E(nx_start:nx_end,ny_start:ny_end,i)))
-    E_row(:,i) = abs(E(1916,:));
-    E_col(:,i) = abs(E(:,1485));
+    E_row(:,i) = abs(E(3102,:));
+    E_col(:,i) = abs(E(:,2613));
+    
+    if i==40
+        max_val = max(max(abs(E)));
+        [max_row,max_col] = find(abs(E)==max_val);
+    elseif i>40
+        max_val2 = max(max(abs(E)));
+        if max_val2>max_val
+            max_val = max_val2;
+            [max_row,max_col] = find(abs(E)==max_val);
+            f_real = z_list(i);
+        end
+    end
+    
+    
+    % plot focal plane
+    if z_list(i)==56e-6
+        E_focal=E;
+        figure(3);
+        imagesc(xA_s,yA_s,abs(E));
+        title("focal plane")
+        xlabel("x");ylabel("y");
+    end
+    
+    % plot gif
+    if plot_gif==true
+        h = figure(2);
+        imagesc(xA_s,yA_s,abs(E(:,:)));
+        title(['z = ',num2str(z_list(i))]);
+        % Capture the plot as an image 
+        frame = getframe(h); 
+        im = frame2im(frame); 
+        [imind,cm] = rgb2ind(im,256); 
+        % Write to the GIF File 
+        if i == 1 
+            imwrite(imind,cm,gif_filename,'gif', 'Loopcount',Inf,'DelayTime',1); 
+        else 
+            imwrite(imind,cm,gif_filename,'gif','WriteMode','append','DelayTime',1); 
+        end
+    end
+    
+    
+    
+    
 end
 toc;
-figure(2);imagesc(z_list,xA_s,E_row);title("rowSlice");ylabel("x");
-figure(3);imagesc(z_list,yA_s,E_col);title("columnSlice");ylabel("y");
-%%
-%{
-h = figure;
-axis tight manual % this ensures that getframe() returns a consistent size
-filename = 'testAnimated.gif';
-for n = 1:nz
- 
-    
-    imagesc(abs(E(:,:,n)))
-    title(['z = ',num2str(z(1,n))])
-    drawnow 
-    % Capture the plot as an image 
-    frame = getframe(h); 
-    im = frame2im(frame); 
-    [imind,cm] = rgb2ind(im,256); 
-    % Write to the GIF File 
-    if n == 1 
-        imwrite(imind,cm,filename,'gif', 'Loopcount',inf); 
-    else 
-        imwrite(imind,cm,filename,'gif','WriteMode','append'); 
-    end 
-end
-
-%}
+figure;imagesc(z_list,xA_s,E_row);title("rowSlice");ylabel("x");
+figure;imagesc(z_list,yA_s,E_col);title("columnSlice");ylabel("y");
 
